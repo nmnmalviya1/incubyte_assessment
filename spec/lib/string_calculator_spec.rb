b7979a34e7f5811e8c9eccfe78f78201ac5ee06a
Naman <nmnmalviya1@gmail.com>
2024-12-22 20:42:27 +0530

    test: add test cases for custom delimiter and negative number handling

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -16,4 +16,12 @@ describe 'StringCalculator' do
   it 'returns number when new line delimiter in numbers string provided' do
     expect(StringCalculator.add("1\n2,3")).to eq(6)
   end
+
+  it 'returns number when custom delimiter fixed in a numbers string provided' do
+    expect(StringCalculator.add("//;\n1;2")).to eq(3)
+  end
+
+  it 'returns error message when negative number string provided' do
+    expect(StringCalculator.add("1,-2,3")).to eq("negative numbers not allowed [-2]")
+  end
 end
